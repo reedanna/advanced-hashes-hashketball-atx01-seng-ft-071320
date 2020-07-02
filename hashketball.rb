1ea3868710c7b0898b6a76d184d3154187264d4a
Anna Nicole Reed <reedanna@mail.com>
2020-07-02 03:06:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -206,9 +206,10 @@ def big_shoe_rebounds
   game_hash.each do |homeOrAway, info|
     game_hash[homeOrAway][:players].each do |player|
       #if player[:shoe] > max_shoe_size
-        player[:shoe] = max_shoe_size
-        player[:rebounds] = max_shoe_rebounds
+        #player[:shoe] = max_shoe_size
+       # player[:rebounds] = max_shoe_rebounds
       #end
+      print player[:shoe]
     end
   end
   max_shoe_size
